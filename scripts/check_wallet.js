dbe04ea6ae715c55f50b7991c8175121f2ccc257
writeprovidence <31359701+writeprovidence@users.noreply.github.com>
2026-06-18 01:01:50 +0100

    Clean up repo and add comprehensive README

--- scripts/check_wallet.js
+++
@@ -1,6 +0,0 @@
-import { ethers } from 'ethers';
-import dotenv from 'dotenv';
-dotenv.config();
-const key = process.env.TREASURY_PRIVATE_KEY;
-const wallet = new ethers.Wallet(key);
-console.log('Wallet Address:', wallet.address);
